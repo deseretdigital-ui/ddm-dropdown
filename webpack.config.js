aa6208340e08d663127989230caa87d2bcf31e6c
Russell Ahlstrom <rahlstrom@deseretdigital.com>
2016-01-20 12:11:51 -0700

    Changes prod to production in webpack define
    To correctly handle the environment it has to be specified as production
    instead of prod.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 
 var definePlugin = new webpack.DefinePlugin({
-  'process.env': {'NODE_ENV': JSON.stringify('prod')}
+  'process.env': {'NODE_ENV': JSON.stringify('production')}
 });
 
 module.exports = {
